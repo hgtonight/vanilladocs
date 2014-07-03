abb090817e51bf5a21c33e28f121c08a618a89ab
kasperisager <kasperisager@gmail.com>
2014-07-02 21:47:35 -0400

    Configure `load-grunt-config` to use `jit-grunt`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,5 +7,8 @@ module.exports = function (grunt) {
 
   require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'tasks')
+  , jitGrunt: {
+      useminPrepare: 'grunt-usemin'
+    }
   });
 };
