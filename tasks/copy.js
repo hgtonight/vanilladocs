e521d62bb66e28414483fa6f14512e38a8e2e6db
kasperisager <kasperisager@gmail.com>
2014-07-07 18:37:36 -0400

    Fix quotes and newlines.
    [ci skip]

--- tasks/copy.js
+++ tasks/copy.js
@@ -4,17 +4,17 @@ module.exports = {
   js: {
     files: [{
       expand: true
-    , cwd: "site/assets/js/"
-    , src: "**/*.js"
-    , dest: ".tmp/assets/"
+    , cwd: 'site/assets/js/'
+    , src: '**/*.js'
+    , dest: '.tmp/assets/'
     }]
   }
 , img: {
     files: [{
       expand: true
-    , cwd: "site/assets/img"
-    , src: "**/*"
-    , dest: "dist/assets/img"
+    , cwd: 'site/assets/img'
+    , src: '**/*'
+    , dest: 'dist/assets/img'
     }]
   }
 };
