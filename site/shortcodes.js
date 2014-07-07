204ce84f00c21517360ca8b5a90e283bad5f6cb2
kasperisager <kasperisager@gmail.com>
2014-07-07 19:00:32 -0400

    Attempt fixing `[code-canvas]` shortcode.

--- site/shortcodes.js
+++ site/shortcodes.js
@@ -3,6 +3,6 @@
 module.exports = [{
   tag: 'code-canvas'
 , cb: function (buf, opts, templateData) {
-    return '<div class="push">' + buf + '</div>';
+    return '<div class="push">\n' + buf + '\n</div>';
   }
 }];
