70962d0df366410f215329455e645b7c68394dbe
kasperisager <kasperisager@gmail.com>
2014-07-08 16:11:08 -0400

    Nuke `[code-canvas]` shortcode. [ci skip]

--- site/shortcodes.js
+++ site/shortcodes.js
@@ -1,8 +1,3 @@
 'use strict';
 
-module.exports = [{
-  tag: 'code-canvas'
-, cb: function (buf, opts, templateData) {
-    return '<div class="push">\n' + buf + '\n</div>';
-  }
-}];
+module.exports = [];
