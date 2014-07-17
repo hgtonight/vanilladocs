e3549c559c9ac563fb96e25fcc32f4038fd98fde
kasperisager <kasperisager@gmail.com>
2014-07-17 16:46:45 -0400

    Use RawGit CDN when deploying.

---
+++ tasks/cdnify.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var url  = require('url')
+  , path = require('path');
+
+module.exports = {
+  options: {
+    base: url.format({
+      protocol: false
+    , hostname: 'cdn.rawgit.com'
+    , pathname: path.join(
+        process.env.GH_OWNER
+      , process.env.GH_PROJECT_NAME
+      , 'gh-pages'
+      )
+    })
+  }
+, dist: {
+    files: [{
+      expand: true
+    , cwd: 'dist'
+    , src: '**/*.html'
+    , dest: 'dist'
+    }]
+  }
+};
